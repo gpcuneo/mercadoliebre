8d186e61f91bd338adcc93ab9a6ecacfcb3ab5b6
Guillermo Pedro Cuneo <guillermo@cuneo.com.ar>
2023-05-08 20:49:00 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use( express.static(publicPath) );
+
+const port = 3333;
+app.listen(port, () => console.log(`Servidor corriendo en el puerto ${port}`) );
+
+
+const index = (req, res) => res.send('Bienvenidooooo');
+
+const home = (req, res) => res.sendFile(path.resolve(__dirname, './views/home.html'));
+const register = (req, res) => res.sendFile(path.resolve(__dirname, './views/register.html'));
+
+app.get('/', index)
+
+app.get('/home', home);
+app.get('/register', register);
+
